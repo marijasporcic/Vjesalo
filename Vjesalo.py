749e0e66ded9af27a0bcfcc8e5d3e7ff3c0f15fb
marijasporcic <106666699+marijasporcic@users.noreply.github.com>
2022-10-22 12:24:50 +0200

    Update Vjesalo.py
    Dodano

--- Vjesalo.py
+++ Vjesalo.py
@@ -3,3 +3,10 @@ import random
 baza_rijeci=["MAČKA", "PAS", "IGRAČKA", "LjEŠNjAK"] 
 abeceda= "Dž - Lj - Nj - A - B - C - Č - Ć - D - Đ - E - F - G - H - I - J - K - L - M - N - O - P - R - S - Š - T - U - V - Z - Ž" 
 abeceda = abeceda. split(" - ") 
+
+neiskoristena_slova = []
+neiskoristena_slova.extend(abeceda) 
+
+tocna_slova = [] 
+netocna_slova = [] 
+izabrana_rijec = "" 
