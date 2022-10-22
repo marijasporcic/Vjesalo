de3dd2283d928109ceba2d05c7fd1e3e9d207dbb
marijasporcic <106666699+marijasporcic@users.noreply.github.com>
2022-10-22 12:07:36 +0200

    Update Vjesalo.py
    Početak programa

--- Vjesalo.py
+++ Vjesalo.py
@@ -1,1 +1,5 @@
-print("M")
+import random
+
+baza_rijeci=["MAČKA", "PAS", "IGRAČKA", "LjEŠNjAK"] 
+abeceda= "Dž - Lj - Nj - A - B - C - Č - Ć - D - Đ - E - F - G - H - I - J - K - L - M - N - O - P - R - S - Š - T - U - V - Z - Ž" 
+abeceda = abeceda. split(" - ") 
