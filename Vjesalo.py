58255d8e4273a6d2bcf2386f0502cc088599f4dd
marijasporcic <106666699+marijasporcic@users.noreply.github.com>
2022-10-22 12:37:02 +0200

    Update Vjesalo.py

--- Vjesalo.py
+++ Vjesalo.py
@@ -4,6 +4,8 @@ baza_rijeci=["MAČKA", "PAS", "IGRAČKA", "LjEŠNjAK"]
 abeceda= "Dž - Lj - Nj - A - B - C - Č - Ć - D - Đ - E - F - G - H - I - J - K - L - M - N - O - P - R - S - Š - T - U - V - Z - Ž" 
 abeceda = abeceda. split(" - ") 
 
+bodovi={}
+
 neiskoristena_slova = []
 neiskoristena_slova.extend(abeceda) 
 
